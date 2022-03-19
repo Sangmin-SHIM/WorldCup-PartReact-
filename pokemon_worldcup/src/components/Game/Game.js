18d06d95753769bfc513315548cdd16d3e511c9f
SangminSHIM <tkdals4723@gmail.com>
2022-03-19 23:18:22 +0100

    add

--- pokemon_worldcup/src/components/Game/Game.js
+++ pokemon_worldcup/src/components/Game/Game.js
@@ -1,16 +1,69 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {FlexBox} from './style'
 
-const Game = ()=>{
+const items = [
+    {
+        name : "bacon",
+        src: require("../../img/bacon.jpg")
+    },
+    {
+        name : "macaron",
+        src: require("../../img/macaron.jpg")
+    },
+    {
+        name : "pancake",
+        src: require("../../img/pancake.jpg")
+    },
+    {
+        name : "pizza",
+        src: require("../../img/pizza.jpg")
+    },
 
-    return <FlexBox>
-        <div className='flex-1'>
-            <img className='food_img' src={require("../../img/bacon.jpg")}/>
-        </div>
-        <div className='flex-1'>
-            <img className='food_img' src={require("../../img/pizza.jpg")}/>
-        </div>
+]
+
+
+const Game = ()=>{
+        const [foods,setFoods] = useState([]);
+        const [displays,setDisplays] = useState([]);
+        const [winners, setWinners] = useState([]);
+
+        const clickHandler = (food) =>()=>{
+             if(foods.length <= 2){
+                if(winners.length === 0){
+                    setDisplays([food]);
+                    alert("Winner is "+ food.name + ' !');
+                } else {
+                    let updatatedFood = [...winners,  food]
+                    setFoods(updatatedFood);
+                    setDisplays([updatatedFood[0], updatatedFood[1]]);
+                    setWinners([]);
+                }
+             } else if(foods.length > 2){
+                setWinners([...winners, food]);
+                setDisplays([foods[2],foods[3]]);
+                setFoods(foods.slice(2));
+             }
+        };
+
+        useEffect(() => {
+            items.sort(()=> Math.random()-0.5);
+            setFoods(items);
+            setDisplays([items[0], items[1]]);
+        }, [])
+    return (
+    <FlexBox>
+        <h1 className='title'>Pokémon</h1>
+        {displays.map((food)=>{
+            return (
+                <div className='flex-1' key={food.name} onClick={clickHandler(food)}>
+                    <img className='food_img' src={food.src}/>
+                    <div className='name'>{food.name}</div>
+                </div>
+            );
+        })}
+
     </FlexBox>
+    )
 }
 
 export default Game;
--- pokemon_worldcup/src/components/Game/style.js
+++ pokemon_worldcup/src/components/Game/style.js
@@ -3,19 +3,33 @@ import styled from "styled-components";
 export const FlexBox = styled.div`
     display:flex;
     flex-wrap: wrap;
-    height:100vh;
+    height:50vh;
+
+    margin:auto;
 
 
+    .title{
+        position:absolute;
+        z-index:999;
+        margin-left:10px;
+        top:10px;
+        transform: translateX(-50%);
+        left:50%;
+        text-transform : uppercase;
+    }
     .flex-1{
+        transform: translateY(50%);
         flex:1;
         min-width:500px;
-        height:100vh;
+        height:50vh;
         overflow:hidden;
+        background-color: black;
+
     }
 
     .food_img{
         width:100%;
-        height:100vh;
+        height:50vh;
         transition: 0.5s;
         cursor:pointer;
 
@@ -23,5 +37,17 @@ export const FlexBox = styled.div`
 
     .food_img:hover{
         transform: scale(1.1);
+        opacity: 0.8;
+    }
+
+    
+    .name {
+        position:absolute;
+        z-index:50;
+        color:#fff;
+        bottom:10%;
+        left:50%;
+        transform: translateX(-50%);
+        font-size:130px;
     }
 `;
